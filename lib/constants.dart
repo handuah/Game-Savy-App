a1051e9e550312e2e23037d9aa4ecb4efe46d1d4
handuah <handuah@gmail.com>
2022-01-10 16:41:25 +0000

    create game search data

--- lib/constants.dart
+++ lib/constants.dart
@@ -73,3 +73,40 @@ List jsonData = [
         'https://www.pngall.com/wp-content/uploads/5/Overwatch-PNG-High-Quality-Image.png'
   },
 ];
+
+List gamesSearchData = [
+  {
+    'name': "Fortnite",
+    'img_url':
+        'https://cdn2.unrealengine.com/s15-evergreen-winterblue-newsheader-1920x1080-4af4edcd3291.jpg',
+    'logo_url':
+        'https://i.dlpng.com/static/png/5441973-fortnite-logo-transparent-fortnite-best-800-v-buck-skins-fortnite-logo-transparent-396_127_preview.png'
+  },
+  {
+    'name': "Mortal Kombat",
+    'img_url':
+        'https://theactionelite.com/wp-content/uploads/2021/04/mortal-kombat-character-posters-social-featured.jpg',
+    'logo_url':
+        'https://www.pngkey.com/png/full/58-587429_krakkens-log-mortal-kombat-x-logo-png.png'
+  },
+  {
+    'name': "Call of Duty",
+    'img_url': 'https://cdn.mos.cms.futurecdn.net/zJiqmQmGfAAiozV2e7f9zQ.jpg',
+    'logo_url':
+        'https://upload.wikimedia.org/wikipedia/commons/4/47/Logo_Franchise_CoD.png'
+  },
+  {
+    'name': "Injustice",
+    'img_url':
+        'https://i.pinimg.com/originals/fd/d2/e1/fdd2e14876d2a444ffada1d81e088fc3.png',
+    'logo_url':
+        'https://cdn2.steamgriddb.com/file/sgdb-cdn/logo/0f28b5d49b3020afeecd95b4009adf4c.png'
+  },
+  {
+    'name': "Infamous",
+    'img_url':
+        'https://www.suckerpunch.com/wp-content/uploads/2017/10/key-art-07b.jpg',
+    'logo_url':
+        'https://preview.redd.it/xgf0kp6bqo251.png?auto=webp&s=e98d18f9f58cfa54fb990151a8e0f130086d7f96'
+  },
+];
